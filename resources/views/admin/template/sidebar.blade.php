98216101bec615e0ff844c897e29c6f07c384f50
fhaladin <farhan.nif.alaudin@gmail.com>
2020-04-14 17:12:09 +0800

    Minor Bug Fix

--- resources/views/admin/template/sidebar.blade.php
+++ resources/views/admin/template/sidebar.blade.php
@@ -2,7 +2,7 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="active">
-          <a href="index.php"><i class="fa fa-dashboard"></i> 
+          <a href="{{ URL('admin') }}"><i class="fa fa-dashboard"></i> 
             <span>Dashboard</span>
           </a>
         </li>
